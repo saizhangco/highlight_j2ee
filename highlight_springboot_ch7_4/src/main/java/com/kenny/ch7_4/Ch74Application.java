02f86f38da5069120c3b52dde42b8ab183cdddce
SaiZhang <zhangsaiyx@163.com>
2019-06-11 22:18:10 +0800

    update project ch7_4

--- highlight_springboot_ch7_4/src/main/java/com/kenny/ch7_4/Ch74Application.java
+++ highlight_springboot_ch7_4/src/main/java/com/kenny/ch7_4/Ch74Application.java
@@ -2,10 +2,18 @@ package com.kenny.ch7_4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Ch74Application {
 
+	@RequestMapping("/")
+	public String hello() {
+		return "hello world!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Ch74Application.class, args);
 	}
